cd41d08f57ec3775722bb6490b242fc251debd5b
Asif Mallick <asifmallick810@gmail.com>
2026-03-23 18:11:32 +0530

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,36 @@
+'''
+This file is an essential part of packaging and diatributing python projects. It is used by setuptools to define the configuration of your project, such as its metadata, dependencies and more.
+'''
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """"
+    This function will return list of requirements
+    """
+    requirement_list:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            ## Read lines from file
+            lines = file.readlines()
+            ## process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e.
+                if requirement and requirement!='-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt is not found")
+
+    return requirement_list
+
+
+setup(
+    name = "NetworkSecurity",
+    version = "0.0.1",
+    author = "Asif Mallick",
+    author_email="asifmallick810@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
